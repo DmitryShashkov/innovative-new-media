da583409b052c4693bfeacec583cd718087b9bea
shashkov.cr <shashkov.cr@gmail.com>
2018-04-04 12:07:16 +0300

    Added title and description

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,5 +1,6 @@
 import {Component} from "@angular/core";
 import {environment} from "../../environments/environment";
+import {Meta, Title} from "@angular/platform-browser";
 
 @Component({
     selector: 'blog',
@@ -8,4 +9,15 @@ import {environment} from "../../environments/environment";
 })
 export class BlogComponent {
     public deployURL = environment.deployURL;
+
+    constructor (
+        private titleService: Title,
+        private metaService: Meta
+    ) {
+        this.titleService.setTitle('Top 10 Most Popular Swift Libraries from GitHub');
+        this.metaService.addTag({
+            name: 'description',
+            content: 'As a team that is directly connected with iOS app development, we keep an eye on all the innovations in the IT industry including libraries. It is not a secret that the usage of libraries can help to save a lot of time. However, which ones to use when it comes to such a relatively new language as Swift? In this article, we are going to present you top 10 Swift libraries on GitHub for iOS development.'
+        })
+    }
 }
